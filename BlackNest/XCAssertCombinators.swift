db4bb22b48d7e2234be9f78c91eda4a902ce4feb
elm4ward <mail@4ward.org>
2016-08-27 00:11:11 +0200

    Level 5

--- BlackNest/XCAssertCombinators.swift
+++ BlackNest/XCAssertCombinators.swift
@@ -113,3 +113,50 @@ public func expectAll<A, B, C>(_ input: C,
     }
     return nil
 }
+
+// --------------------------------------------------------------------------------
+// MARK: - 5 Level
+// --------------------------------------------------------------------------------
+
+@discardableResult
+public func expectAll<A, B, C>(_ input: C,
+                      in breeding: B,
+                      is expected: A,
+                      line: UInt = #line,
+                      file: StaticString = #file) -> B.BB.BB.BB.BB.BO?
+  where
+  A: BLNBranchable,
+  B: BLNBranchable,
+  A.BB: BLNBranchable,
+  A.BB.BB: BLNBranchable,
+  A.BB.BB.BB: BLNBranchable,
+  B.BB: BLNBranchable,
+  B.BB.BB: BLNBranchable,
+  B.BB.BB.BB: BLNBranchable,
+  B.BA: BLNBreedable,
+  B.BB.BA: BLNBreedable,
+  B.BB.BB.BA: BLNBreedable,
+  B.BB.BB.BB.BA: BLNBreedable,
+  B.BB.BB.BB.BB: BLNBreedable,
+  B.BA.BI == C,
+  B.BA.BE == A.BA,
+  B.BB.BA.BI == B.BA.BO,
+  B.BB.BA.BE == A.BB.BA,
+  B.BB.BB.BA.BI == B.BB.BA.BO,
+  B.BB.BB.BA.BE == A.BB.BB.BA,
+  B.BB.BB.BB.BA.BI == B.BB.BB.BA.BO,
+  B.BB.BB.BB.BA.BE == A.BB.BB.BB.BA,
+  B.BB.BB.BB.BB.BI == B.BB.BB.BB.BA.BO,
+  B.BB.BB.BB.BB.BE == A.BB.BB.BB.BB {
+    do {
+      let m1 = try breeding.left.breeding(input, expected.left)
+      let m2 = try breeding.right.left.breeding(m1, expected.right.left)
+      let m3 = try breeding.right.right.left.breeding(m2, expected.right.right.left)
+      let m4 = try breeding.right.right.right.left.breeding(m3, expected.right.right.right.left)
+      let m5 = try breeding.right.right.right.right.breeding(m4, expected.right.right.right.right)
+      return m5
+    } catch let error {
+      XCTAssert(false, "\(error)", file: file, line: line)
+    }
+    return nil
+}
