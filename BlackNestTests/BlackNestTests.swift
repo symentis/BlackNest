dcf2b359196956a34f4b1b4d5daa7c9a3adf8a02
Elmar Kretzer <elkr@symentis.com>
2016-09-09 15:10:11 +0200

    >_< fix dat test

--- BlackNestTests/BlackNestTests.swift
+++ BlackNestTests/BlackNestTests.swift
@@ -168,8 +168,8 @@ class BlackNestTests: XCTestCase {
 
     expect(("Burt", nil, 100) |  birdWatcher => ("Burt", nil, 100, "Burt"))
     expect(("Burt", 20, 100)  |  birdWatcher => ("Burt", 20, 100, "Burt - The Master."))
-    expect(("Burt", 20, 0)    |  birdWatcher => ("Burt", 0, 100, "Burt - The Talent."))
-    expect(("Burt", nil, 0)   |  birdWatcher => ("Burt", 0, 0, "Burt - The Bloody Rookie."))
+    expect(("Burt", 20, 10)   |  birdWatcher => ("Burt", 20, 10, "Burt"))
+    expect(("Burt", 1, 0)     |  birdWatcher => ("Burt", 1, 0, "Burt - The Rookie."))
   }
 
 }
