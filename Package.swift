0cd10b78dfd6849445a3a1206e7cde45ca8b2449
Felix Scheinost <fesc@symentis.com>
2020-08-26 17:13:16 +0200

    Add Package.swift; Generate xcodeproj

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BlackNest",
+    products: [
+        .library(
+            name: "BlackNest",
+            targets: ["BlackNest"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "BlackNest",
+            dependencies: [],
+            path: "BlackNest"
+        ),
+        .testTarget(
+            name: "BlackNestTests",
+            dependencies: ["BlackNest"],
+            path: "BlackNestTests"
+        ),
+    ]
+)
